20126e92dd6342a7f89a106e76a0efb0f83fe70f
Abir Ahsan <abirahsan122@gmail.com>
2023-06-23 00:35:38 +0600

    download file system done

--- lib/services/file_downloader_service.dart
+++ lib/services/file_downloader_service.dart
@@ -1,9 +1,6 @@
 import 'package:background_downloader/background_downloader.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
-
-import 'custom_eassy_loading.dart';
 
 class FileDownloaderService {
   void myNotificationTapCallback(Task task, NotificationType notificationType) {
