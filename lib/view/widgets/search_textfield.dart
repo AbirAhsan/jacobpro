72e0f6e12480bf02f07bf540202f1c01f21c52c2
Abir Ahsan <abirahsan122@gmail.com>
2023-08-08 22:56:44 +0600

    search suggestion done when data is available

--- lib/view/widgets/search_textfield.dart
+++ lib/view/widgets/search_textfield.dart
@@ -5,7 +5,6 @@ import '../../controller/estimate_controller.dart';
 import '../../controller/search_controller.dart';
 import '../../model/service_and_material_item_model.dart';
 import '../../services/debouncher_service.dart';
-import '../variables/icon_variables.dart';
 import '../variables/text_style.dart';
 import 'custom_text_field.dart';
 
@@ -88,84 +87,67 @@ class _ItemSearchTextFieldState extends State<ItemSearchTextField> {
   }
 }
 
-Widget buildOverlay() => Material(
-      elevation: 8,
-      child: SizedBox(
-        height: Get.height / 3,
-        child: GetBuilder<SearchControl>(
-            init: SearchControl(),
-            builder: (searchCtrl) {
-              if (searchCtrl.isSearching.value) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 50,
-                      width: 50,
-                      child: Image.asset(
-                        CustomIcons.loader,
-                      ),
-                    ),
-                    const Text("Searching .....")
-                  ],
-                );
-              }
-              return searchCtrl.searchDataList.isNotEmpty
-                  ? ListView.builder(
-                      padding: const EdgeInsets.all(15.0),
-                      shrinkWrap: true,
-                      physics: const ScrollPhysics(),
-                      itemCount: searchCtrl.searchDataList.length,
-                      itemBuilder: (buidContext, index) {
-                        ServiceandMaterialItemModel? searchData =
-                            searchCtrl.searchDataList[index];
-                        return Card(
-                          child: GetBuilder<EstimatedController>(
-                              init: EstimatedController(),
-                              builder: (estimatedCtrl) {
-                                return ListTile(
-                                  // search type 1 for topic , 2 for consultant
+Widget buildOverlay() => GetBuilder<SearchControl>(
+    init: SearchControl(),
+    builder: (searchCtrl) {
+      return Visibility(
+        visible: searchCtrl.searchDataList.isNotEmpty,
+        child: Material(
+          elevation: 8,
+          child: SizedBox(
+            height: Get.height / 3,
+            child: ListView.builder(
+              padding: const EdgeInsets.all(15.0),
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              itemCount: searchCtrl.searchDataList.length,
+              itemBuilder: (buidContext, index) {
+                ServiceandMaterialItemModel? searchData =
+                    searchCtrl.searchDataList[index];
+                return Card(
+                  child: GetBuilder<EstimatedController>(
+                      init: EstimatedController(),
+                      builder: (estimatedCtrl) {
+                        return ListTile(
+                          // search type 1 for topic , 2 for consultant
 
-                                  title: Text(
-                                    searchData!.itemName ?? "",
-                                    style: CustomTextStyle.normalBoldStyleBlack,
-                                  ),
-                                  subtitle:
-                                      Text(searchData.itemDescription ?? ""),
+                          title: Text(
+                            searchData!.itemName ?? "",
+                            style: CustomTextStyle.normalBoldStyleBlack,
+                          ),
+                          subtitle: Text(searchData.itemDescription ?? ""),
 
-                                  onTap: () async {
-                                    await searchCtrl.hideOverlay();
-                                    searchCtrl.searchTextEditingCtrl!.text =
-                                        searchData.itemName!;
-
-                                    estimatedCtrl
-                                        .addDataToServiceAndMaterialItemModel(
-                                            searchData);
-                                    // if (searchData.type == 2) {
-                                    //   PageNavigationService.generalNavigation(
-                                    //     "/ConsultantProfileScreen",
-                                    //     arguments: searchData.id,
-                                    //   );
-                                    // } else if (searchData.type == 1) {
-                                    //   if (searchData.topicMarker == "_country_") {
-                                    //     PageNavigationService.generalNavigation(
-                                    //         "/CountryDetailsScreen",
-                                    //         arguments: searchData.id);
-                                    //   } else if (searchData.topicMarker ==
-                                    //       "_event_") {
-                                    //     PageNavigationService.generalNavigation(
-                                    //         "/EventDetailsScreen",
-                                    //         arguments: searchData.id);
-                                    //   }
-                                    // }
-                                  },
-                                );
-                              }),
+                          onTap: () async {
+                            await searchCtrl.hideOverlay();
+                            searchCtrl.searchTextEditingCtrl!.text =
+                                searchData.itemName!;
+                            searchCtrl.searchDataList.clear();
+                            estimatedCtrl.addDataToServiceAndMaterialItemModel(
+                                searchData);
+                            // if (searchData.type == 2) {
+                            //   PageNavigationService.generalNavigation(
+                            //     "/ConsultantProfileScreen",
+                            //     arguments: searchData.id,
+                            //   );
+                            // } else if (searchData.type == 1) {
+                            //   if (searchData.topicMarker == "_country_") {
+                            //     PageNavigationService.generalNavigation(
+                            //         "/CountryDetailsScreen",
+                            //         arguments: searchData.id);
+                            //   } else if (searchData.topicMarker ==
+                            //       "_event_") {
+                            //     PageNavigationService.generalNavigation(
+                            //         "/EventDetailsScreen",
+                            //         arguments: searchData.id);
+                            //   }
+                            // }
+                          },
                         );
-                      },
-                    )
-                  : const Center(child: Text("No Data Found"));
-            }),
-      ),
-    );
+                      }),
+                );
+              },
+            ),
+          ),
+        ),
+      );
+    });
